c96a3e0e5c4f49ca8c0da648ded6125ecb0e5859
Logan Ferris <FLogan800@gmail.com>
2025-07-19 00:20:52 -0500

    Create enums for sort algorithms and array distribution types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use sorting_algorithm::*;
+use strum_macros::EnumIter;
+
+#[derive(EnumIter)]
+enum Algorithm {
+    BubbleSort,
+    GnomeSort,
+    InsertionSort,
+    QuickSort,
+    SelectionSort,
+    ShellSort,
+}
+
+#[derive(EnumIter)]
+enum DistributionType {
+    Random,
+    Repeated,
+    Reversed,
+    Sorted,
+    NearlySorted,
+}
+
 fn main() {
     println!("Hello, world!");
 }
